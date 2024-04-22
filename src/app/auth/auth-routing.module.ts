5657af6a10f651c2a997fc688357b9a0d71b165b
Ricardo Espinoza <respinoza@mgcertifica.cl>
2024-04-22 18:58:58 -0400

    Pages

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = []
+
+@NgModule({
+  imports: [
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
+  ],
+  providers: [
+
+  ],
+})
+export class AppRoutingModule { }
